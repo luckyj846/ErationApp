896c9fc4748d01e2a2f4165fd6275c582e52e330
unknown <luckyj846@gmail.com>
2021-03-02 17:11:11 +0530

    layout pe work kiya

--- app/src/main/java/com/example/rationcard/model/Rationcard.java
+++ app/src/main/java/com/example/rationcard/model/Rationcard.java
@@ -21,31 +21,37 @@ public class Rationcard
     }
 
     public void setId(int id)
+
     {
         this.id = id;
     }
 
     public String getName()
+
     {
         return name;
     }
 
     public void setName(String name)
+
     {
         this.name = name;
     }
 
     public String getSubarea()
+
     {
         return subarea;
     }
 
     public void setSubarea(String subarea)
+
     {
         this.subarea = subarea;
     }
 
     public String getArea()
+
     {
         return area;
     }
@@ -56,11 +62,13 @@ public class Rationcard
     }
 
     public String getAddress()
+
     {
         return address;
     }
 
     public void setAddress(String address)
+
     {
         this.address = address;
     }
@@ -76,16 +84,19 @@ public class Rationcard
     }
 
     public String getCity()
+
     {
         return city;
     }
 
     public void setCity(String city)
+
     {
         this.city = city;
     }
 
     public String getCategory()
+
     {
         return category;
     }
@@ -96,21 +107,25 @@ public class Rationcard
     }
 
     public String getState()
+
     {
         return state;
     }
 
     public void setState(String state)
+
     {
         this.state = state;
     }
 
     public String getShop()
+
     {
         return shop;
     }
 
     public void setShop(String shop)
+
     {
         this.shop = shop;
     }
@@ -121,16 +136,19 @@ public class Rationcard
     }
 
     public void setEmail(String email)
+
     {
         this.email = email;
     }
 
     public int getContact()
+
     {
         return contact;
     }
 
     public void setContact(int contact)
+
     {
         this.contact = contact;
     }
